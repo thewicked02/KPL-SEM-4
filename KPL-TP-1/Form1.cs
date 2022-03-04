92edc9f221fbbbc3a33f4041da996f40c71caedc
thewicked02 <100459594+thewicked02@users.noreply.github.com>
2022-03-04 16:16:02 +0700

    Add files via upload

---
+++ KPL-TP-1/Form1.cs
@@ -0,0 +1,20 @@
+namespace KPL_TP_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text; 
+        }
+    }
+}
